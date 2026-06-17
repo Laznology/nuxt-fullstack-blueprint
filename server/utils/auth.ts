5840e9890f4afb387c8b3ffef890a75270cd825d
LazTech <160091267+Laznology@users.noreply.github.com>
2026-06-17 06:43:26 +0000

    feat: Configure and extend server authentication logic

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,16 +1,25 @@
+import { apiKey } from "@better-auth/api-key";
 import { db } from "@nuxthub/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { admin } from "better-auth/plugins";
+import { admin, anonymous, jwt, openAPI, username } from "better-auth/plugins";
+import { auditLog } from "better-auth-audit-logs";
 
 export const auth = betterAuth({
 	/** other drizzle config can see at https://better-auth.com/docs/adapters/drizzle */
 	database: drizzleAdapter(db, {
-		provider: "pg",
+		provider: "sqlite",
 	}),
 	emailAndPassword: {
 		enabled: true,
 	},
-	plugins: [admin()],
-	usePlural: true,
+	plugins: [
+		admin(),
+		apiKey(),
+		username(),
+		anonymous(),
+		jwt(),
+		auditLog(),
+		openAPI(),
+	],
 });
