5e7ae09cecfd47e4fc28d5e2b202c61b2bc049c7
LazTech <160091267+Laznology@users.noreply.github.com>
2026-06-17 06:43:26 +0000

    feat: Configure auth client with multiple plugins

--- app/utils/auth-client.ts
+++ app/utils/auth-client.ts
@@ -1,4 +1,20 @@
+import { apiKeyClient } from "@better-auth/api-key/client";
+import {
+	adminClient,
+	anonymousClient,
+	jwtClient,
+	usernameClient,
+} from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/vue";
-
-export const authClient = createAuthClient();
+import { auditLogClient } from "better-auth-audit-logs/client";
+export const authClient = createAuthClient({
+	plugins: [
+		apiKeyClient(),
+		adminClient(),
+		usernameClient(),
+		anonymousClient(),
+		jwtClient(),
+		auditLogClient(),
+	],
+});
 export const { signIn, signUp, signOut, useSession } = authClient;
